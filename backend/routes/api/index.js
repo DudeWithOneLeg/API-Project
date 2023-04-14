07f72b750f52a06d0d1bed2d62859b2c9c03b828
dudewithoneleg <galvancromeo@gmail.com>
2023-04-14 17:44:15 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,8 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser, requireAuth } = require('../../utils/auth.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
@@ -19,7 +21,7 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
-router.get('/api/csrf/restore', (req, res) => {
+router.get('/csrf/restore', (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
@@ -49,4 +51,12 @@ router.get(
 );
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 module.exports = router;
