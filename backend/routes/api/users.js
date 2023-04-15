cec00844510b40bc7992aa0dc739b43e3bd5facb
dudewithoneleg <galvancromeo@gmail.com>
2023-04-15 17:34:24 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,23 +4,67 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const user = require('../../db/models/user');
 const bcrypt = require('bcryptjs')
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
-router.post('/', async (req, res) => {
-    const {email, username, password} = await req.body;
-    console.log(email, username, password)
-    const hashedPassword = bcrypt.hashSync(password)
-    const user = await User.create({email, username, hashedPassword})
-    const safeUser = {
+// router.post('/', async (req, res) => {
+//     const {email, username, password} = await req.body;
+//     console.log(email, username, password)
+//     const hashedPassword = bcrypt.hashSync(password)
+//     const user = await User.create({email, username, hashedPassword})
+//     const safeUser = {
+//         id: user.id,
+//         email: user.email,
+//         username: user.username
+//     };
+
+//     await setTokenCookie(res, safeUser);
+
+//     return res.json({
+//         user: safeUser
+//     })
+// })
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+  router.post(
+    '/',
+    validateSignup,
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+
+      const safeUser = {
         id: user.id,
         email: user.email,
-        username: user.username
-    };
+        username: user.username,
+      };
 
-    await setTokenCookie(res, safeUser);
+      await setTokenCookie(res, safeUser);
 
-    return res.json({
+      return res.json({
         user: safeUser
-    })
-})
+      });
+    }
+  );
 
 module.exports = router;
